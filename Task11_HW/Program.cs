b2da97b3501d74ee9fd7edc18ecc6ba4e48a15c3
Elena Sheveleva <lahjalena@gmail.com>
2023-02-02 17:34:54 +0100

    добавлено задание 13  из ДЗ семинара 2

---
+++ Task11_HW/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+
+while (N > 999)
+{
+   N = N / 10;
+}
+int count = 10;
+int result = N % count;
+
+if (N < 100)
+{
+    Console.WriteLine("В веденном числе отсутствует третья цифра");
+}
+else
+{
+    N = N % 10;
+    Console.WriteLine($"В веденном числе третья цифра = {N}");
+}
