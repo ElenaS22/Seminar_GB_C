0337265a1a6ad226a50e7302f21c7f4fb4162adf
Elena Sheveleva <lahjalena@gmail.com>
2023-02-09 17:48:00 +0100

    added task 27 HW to seminar 4

---
+++ Task24_HW/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int Summa(int N)
+{
+int S = 0;
+  while (N > 0)
+{
+S = S + (N % 10);
+N = N / 10;
+}
+return S;
+}
+Console.WriteLine("Введите число N ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма цифр в числе {N} составляет {Summa(N)}");
