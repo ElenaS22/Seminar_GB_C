79bc4478ce97dcebc6c11b76c4653adce54ffb08
Elena Sheveleva <lahjalena@gmail.com>
2023-02-09 18:21:54 +0100

    changes

--- Task23_HW/Program.cs
+++ Task23_HW/Program.cs
@@ -2,24 +2,40 @@
 
 Console.Clear();
 
-int Expo(int A, int B) 
-{ 
-int A1 = 1;
-int i = 1;
-while (i <= B && B != 0 && B !=1)
-   {
-        A1 = A * A1;
-        i++;
-    }
-if (B == 1)
-{
-    A1 = A;
-}
-    return A1;   
-}
-Console.WriteLine("Введите число A ");
-int A = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите число B ");
-int B = int.Parse(Console.ReadLine()!);
+// int Expo(int A, int B)
+// {
+//     int A1 = 1;
+//     int i = 1;
+//     while (i <= B && B != 0 && B != 1)
+//     {
+//         A1 = A * A1;
+//         i++;
+//     }
+//     if (B == 1)
+//     {
+//         A1 = A;
+//     }
+//     return A1;
+// }
+// Console.WriteLine("Введите число A ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число B ");
+// int B = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine($"Число {A} в степени {B} составит {Expo(A, B)}");
+// Console.WriteLine($"Число {A} в степени {B} составит {Expo(A, B)}");
+
+int Powe(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+
+Console.Write($"Введите A: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write($"Введите B: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{a}^{b} = {Powe(a, b)}");
