506fdd471d83b7a18a6818a364697b94a82eaa13
Elena Sheveleva <lahjalena@gmail.com>
2023-02-12 18:42:39 +0100

    added task from seminar 5

---
+++ Task27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+//Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+//Найдите сумму отрицательных и положительных элементов массива.
+
+Console.Clear();
+
+int[] GetArray(int size, int MinValue, int MaxValue)
+{
+    int[] Result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Result[i] = new Random().Next(MinValue, MaxValue + 1);
+    }
+    return Result;
+}
+
+void SigChange(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(", ", array));
+SigChange(array);
+Console.WriteLine(String.Join(", ", array));
