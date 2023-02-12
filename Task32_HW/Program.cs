dd6cd43382a57e176fe3038d63e87f42dcd153b7
Elena Sheveleva <lahjalena@gmail.com>
2023-02-12 18:45:29 +0100

    added task36 HW from seminar 5

---
+++ Task32_HW/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+Console.Clear();
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int[] array = GetArray(5, -10, 10);
+Console.WriteLine(String.Join(", ", array));
+
+int Sum_Odd = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    // Console.WriteLine($"{i%2}");
+    if ((i % 2) != 0)
+    {
+        Sum_Odd = Sum_Odd + array[i];
+    }
+}
+
+Console.WriteLine($"Сумма нечетных элементов массива равна {Sum_Odd}");
