e5e7fc4d9aaaaee9e0a3e410c0b14970a612a248
Elena Sheveleva <lahjalena@gmail.com>
2023-02-22 15:22:26 +0100

    Задача 46

---
+++ Task41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Clear();
+
+int[,] GetArray(int m, int n, int MinValue, int MaxValue)
+{
+    int[,] result = new int[m,n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(MinValue, MaxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    {
+    
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+         Console.WriteLine();
+    }
+   }
+}
+
+Console.Write("row =  ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("col =  ");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] array2D = GetArray(row,col, -10,10);
+PrintArray(array2D);
